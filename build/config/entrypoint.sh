f725b533e13d9816aaed70ef6f3aa238538ed877
stefaan <stefaan@ponnet.com>
2022-01-07 14:53:53 +0100

    first release

---
+++ build/config/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Start nginx"
+
+nginx
+
+echo "Generating auth token"
+mkdir -p "/root/.eth2validators"
+
+# remove old token if it's there
+rm -f /root/.eth2validators/auth-token
+
+# generate new token
+validator web generate-auth-token --wallet-dir=/root/.eth2validators --accept-terms-of-use
+
+# remove old token if it's there
+rm -f /usr/share/nginx/wizard/auth-token.txt
+
+# copy new token to wizard for authentication link
+cat /root/.eth2validators/auth-token | tail -1 > /usr/share/nginx/wizard/auth-token.txt
+chmod 644 /usr/share/nginx/wizard/auth-token.txt
+
+exec -c validator \
+    --datadir=/root/.eth2 \
+    --rpc-host 0.0.0.0 \
+    --monitoring-host 0.0.0.0 \
+    --beacon-rpc-provider="prysm-gnosis-beacon-chain.avado.dnp.dappnode.eth:4000" \
+    --beacon-rpc-gateway-provider="prysm-gnosis-beacon-chain.avado.dnp.dappnode.eth:3500" \
+    --wallet-dir=/root/.eth2validators \
+    --config-file /root/sbc/config/config.yml \
+    --chain-config-file /root/sbc/config/config.yml \
+    --wallet-password-file /root/.eth2wallets/wallet-password.txt \
+    --write-wallet-password-on-web-onboarding \
+    --graffiti="AVADO!" \
+    --web \
+    --grpc-gateway-host=0.0.0.0 \
+    --grpc-gateway-port=80 \
+    --accept-terms-of-use \
+    ${EXTRA_OPTS}
+
